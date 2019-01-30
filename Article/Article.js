4c3442fd1729e5e9f7cbc32adef7c03631a49a4e
Jamie Goodnight <goodnightlambda@gmail.com>
2019-01-30 16:19:39 -0500

    I think I'm doing stretch. I'm actually not sure, I just tried to add some animations, which is probably the stretch!

--- Article/Article.js
+++ Article/Article.js
@@ -10,13 +10,19 @@ class Article {
     this.expandButton.textContent="expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
-  }
-
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+  } 
+  expandArticle(){
+    TweenMax.to(this.domElement, 2, {
+      height: 400,
+      ease: Bounce.easeOut
+    });
   }
 }
+//   expandArticle() {
+//     // Using our reference to the domElement, toggle a class to expand or hide the article.
+//     this.domElement.classList.toggle('article-open');
+//   }
+// }
 
 
 
