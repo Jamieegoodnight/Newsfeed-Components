d3152b57d455a253e3b6c1b1f8fe334f1859bde5
Jamie Goodnight <goodnightlambda@gmail.com>
2019-01-30 18:49:30 -0500

    Added an animation to the article collapse

--- Article/Article.js
+++ Article/Article.js
@@ -7,18 +7,39 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton=domElement.querySelector('.expandButton');
     this.btnOpn=domElement.querySelector('.btn-opn');
-    this.btnCls=domElement.querySelector('.btn-cls')
+    this.btnCls=domElement.querySelector('.btn-cls');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.btnOpn.textContent="expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.btnOpn.addEventListener('click', () => this.expandArticle());
+    this.btnCls.addEventListener('click', () => this.collapseArticle());
   } 
   expandArticle(){
-    this.domElement.classList.toggle('article-open');
-    this.btnOpn.classList.toggle('hidden-btn')
+    TweenMax.to(this.domElement, 2, {
+      height: 400,
+      ease: Bounce.easeOut
+    });
+    this.btnOpn.classList.toggle('hidden-btn');
+    this.btnCls.classList.toggle('hidden-btn');
+  }
+  collapseArticle(){
+    TweenMax.to(this.domElement, 2, {
+      height: 50,
+      ease: Bounce.easeOut
+    });
+    this.btnOpn.classList.toggle('hidden-btn');
     this.btnCls.classList.toggle('hidden-btn');
   }
 }
+
+// expandArticle(){
+//   this.domElement.classList.toggle('article-open');
+//   this.btnOpn.classList.toggle('hidden-btn')
+//   this.btnCls.classList.toggle('hidden-btn');
+// }
+// }
+
+
 //   expandArticle() {
 //     // Using our reference to the domElement, toggle a class to expand or hide the article.
 //     this.domElement.classList.toggle('article-open');
